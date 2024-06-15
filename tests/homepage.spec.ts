93097e9b0ed3baf1e92541f93b790d9724d9a5a9
Daniel J <daniel19970222@gmail.com>
2024-06-15 02:27:26 +0200

    test case title

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -117,7 +117,7 @@ test.describe('Test cases based on excel file', () => {
 
     await newPageThird.close();
   });
-  test('As a user, I want to see available products to purchase', async ({ loginPage, homePage }) => {
+  test('TC_009 | As a user, I want to see available products to purchase', async ({ loginPage, homePage }) => {
 
     await loginPage.loginAsUser(CorrectUser.login, CorrectUser.password);
 
